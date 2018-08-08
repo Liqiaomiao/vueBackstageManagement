a8bee3b750f1a77f042eabea22858b1a3e66f0c7
Liqiaomiao <1049259465@qq.com>
2018-08-08 15:32:55 +0800

    热力图根据需要形成x,y坐标名称及对应的数据值；增加值修改弹窗 并总结更改信息值；

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,8 @@ export default {
 </script>
 
 <style >
+  .dialogform1{
+    width: 350px;
+    margin: 0 auto;
+  }
 </style>
--- src/components/system/echartDemo.vue
+++ src/components/system/echartDemo.vue
@@ -1,96 +1,378 @@
 <template>
-    <div>
-      echarts
-        <div id="main">
+  <div>
+    {{values}}
+    <div ref="pcicontainer" id="main">
 
-        </div>
     </div>
+    <el-dialog
+      title=" 修改票价"
+      :visible.sync="dialogVisible"
+      append-to-body
+      width="500px"
+    >
+      <el-form ref="form" :model="form" label-width="60px" size="small" class="dialogform1">
+        <el-form-item label="上车站">
+          <el-input v-model="form.upname" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="下车站">
+          <el-input v-model="form.downname" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="价格">
+          <el-input v-model="form.price"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="confirm">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-   let echarts = require('echarts');
-   // 基于准备好的dom，初始化echarts实例
+  let echarts = require('echarts');
+  let   myChart,dom;
+  // 基于准备好的dom，初始化echarts实例
+  let stations = {
+    "obj": [{
+      "delFlag": 0,
+      "id": "b76b6843be194a2d97772d7751dbd3e7",
+      "lineCode": "10",
+      "lineId": "a45a22158e0f43709b7bba958765bb37",
+      "orderNo": 1,
+      "stationAlias": "地铁1站",
+      "stationId": 201,
+      "stationName": "地铁1站",
+      "upDown": 1
+    }, {
+      "delFlag": 0,
+      "id": "3417bc231c4e49fca57f6275269c551b",
+      "lineCode": "10",
+      "lineId": "a45a22158e0f43709b7bba958765bb37",
+      "orderNo": 2,
+      "stationAlias": "地铁2站",
+      "stationId": 202,
+      "stationName": "地铁2站",
+      "upDown": 1
+    }, {
+      "delFlag": 0,
+      "id": "e47ff28e77ab47c5a334ba1f46ae5ae6",
+      "lineCode": "10",
+      "lineId": "a45a22158e0f43709b7bba958765bb37",
+      "orderNo": 3,
+      "stationAlias": "地铁3站",
+      "stationId": 203,
+      "stationName": "地铁3站",
+      "upDown": 1
+    }, {
+      "delFlag": 0,
+      "id": "552cc4b6f2fe40b5be60a53eb5bd727d",
+      "lineCode": "10",
+      "lineId": "a45a22158e0f43709b7bba958765bb37",
+      "orderNo": 4,
+      "stationAlias": "地铁4站",
+      "stationId": 204,
+      "stationName": "地铁4站",
+      "upDown": 1
+    }, {
+      "delFlag": 0,
+      "id": "b9d92e6470f1475fa5e820ad77f86761",
+      "lineCode": "10",
+      "lineId": "a45a22158e0f43709b7bba958765bb37",
+      "orderNo": 5,
+      "stationAlias": "地铁5站",
+      "stationId": 205,
+      "stationName": "地铁5站",
+      "upDown": 1
+    }, {
+      "delFlag": 0,
+      "id": "079d90db5c22472887ec6be8a8256c16",
+      "lineCode": "10",
+      "lineId": "a45a22158e0f43709b7bba958765bb37",
+      "orderNo": 6,
+      "stationAlias": "地铁6站",
+      "stationId": 206,
+      "stationName": "地铁6站",
+      "upDown": 1
+    }],
+    "status": true
+  }
+  let pricelist = {
+    "obj": [{
+      "downId": "079d90db5c22472887ec6be8a8256c16",
+      "downNo": "6",
+      "isDisabled": 0,
+      "price": "4.00",
+      "upId": "3417bc231c4e49fca57f6275269c551b",
+      "upNo": "2"
+    }, {
+      "downId": "552cc4b6f2fe40b5be60a53eb5bd727d",
+      "downNo": "4",
+      "isDisabled": 0,
+      "price": "2.00",
+      "upId": "3417bc231c4e49fca57f6275269c551b",
+      "upNo": "2"
+    }, {
+      "downId": "b9d92e6470f1475fa5e820ad77f86761",
+      "downNo": "5",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "3417bc231c4e49fca57f6275269c551b",
+      "upNo": "2"
+    }, {
+      "downId": "e47ff28e77ab47c5a334ba1f46ae5ae6",
+      "downNo": "3",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "3417bc231c4e49fca57f6275269c551b",
+      "upNo": "2"
+    }, {
+      "downId": "079d90db5c22472887ec6be8a8256c16",
+      "downNo": "6",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "552cc4b6f2fe40b5be60a53eb5bd727d",
+      "upNo": "4"
+    }, {
+      "downId": "b9d92e6470f1475fa5e820ad77f86761",
+      "downNo": "5",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "552cc4b6f2fe40b5be60a53eb5bd727d",
+      "upNo": "4"
+    }, {
+      "downId": "079d90db5c22472887ec6be8a8256c16",
+      "downNo": "6",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "b76b6843be194a2d97772d7751dbd3e7",
+      "upNo": "1"
+    }, {
+      "downId": "3417bc231c4e49fca57f6275269c551b",
+      "downNo": "2",
+      "isDisabled": 0,
+      "price": "3.00",
+      "upId": "b76b6843be194a2d97772d7751dbd3e7",
+      "upNo": "1"
+    }, {
+      "downId": "552cc4b6f2fe40b5be60a53eb5bd727d",
+      "downNo": "4",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "b76b6843be194a2d97772d7751dbd3e7",
+      "upNo": "1"
+    }, {
+      "downId": "b9d92e6470f1475fa5e820ad77f86761",
+      "downNo": "5",
+      "isDisabled": 0,
+      "price": "4.00",
+      "upId": "b76b6843be194a2d97772d7751dbd3e7",
+      "upNo": "1"
+    }, {
+      "downId": "e47ff28e77ab47c5a334ba1f46ae5ae6",
+      "downNo": "3",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "b76b6843be194a2d97772d7751dbd3e7",
+      "upNo": "1"
+    }, {
+      "downId": "079d90db5c22472887ec6be8a8256c16",
+      "downNo": "6",
+      "isDisabled": 0,
+      "price": "5.00",
+      "upId": "b9d92e6470f1475fa5e820ad77f86761",
+      "upNo": "5"
+    }, {
+      "downId": "079d90db5c22472887ec6be8a8256c16",
+      "downNo": "6",
+      "isDisabled": 0,
+      "price": "1.00",
+      "upId": "e47ff28e77ab47c5a334ba1f46ae5ae6",
+      "upNo": "3"
+    }, {
+      "downId": "552cc4b6f2fe40b5be60a53eb5bd727d",
+      "downNo": "4",
+      "isDisabled": 0,
+      "price": "3.00",
+      "upId": "e47ff28e77ab47c5a334ba1f46ae5ae6",
+      "upNo": "3"
+    }, {
+      "downId": "b9d92e6470f1475fa5e820ad77f86761",
+      "downNo": "5",
+      "isDisabled": 0,
+      "price": "5.00",
+      "upId": "e47ff28e77ab47c5a334ba1f46ae5ae6",
+      "upNo": "3"
+    }],
+    "status": true
+  };
+  let xline = [];
+  let yline = [];
+  let values = [];
 
-   export default {
-        name: "echartDemo",
-        mounted(){
-          var dom = document.getElementById("main");
-          var myChart = echarts.init(dom);
-          var app = {};
-          var option = null;
-          app.title = '笛卡尔坐标系上的热力图';
-
-          var hours = ['12a', '1a', '2a', '3a', '4a', '5a', '6a',
-            '7a', '8a', '9a','10a','11a',
-            '12p', '1p', '2p', '3p', '4p', '5p',
-            '6p', '7p', '8p', '9p', '10p', '11p'];
-          var days = ['Saturday', 'Friday', 'Thursday',
-            'Wednesday', 'Tuesday', 'Monday', 'Sunday'];
-
-          var data = [[0,0,5],[0,1,1],[0,2,0],[0,3,0],[0,4,0],[0,5,0],[0,6,0],[0,7,0],[0,8,0],[0,9,0],[0,10,0],[0,11,2],[0,12,4],[0,13,1],[0,14,1],[0,15,3],[0,16,4],[0,17,6],[0,18,4],[0,19,4],[0,20,3],[0,21,3],[0,22,2],[0,23,5],[1,0,7],[1,1,0],[1,2,0],[1,3,0],[1,4,0],[1,5,0],[1,6,0],[1,7,0],[1,8,0],[1,9,0],[1,10,5],[1,11,2],[1,12,2],[1,13,6],[1,14,9],[1,15,11],[1,16,6],[1,17,7],[1,18,8],[1,19,12],[1,20,5],[1,21,5],[1,22,7],[1,23,2],[2,0,1],[2,1,1],[2,2,0],[2,3,0],[2,4,0],[2,5,0],[2,6,0],[2,7,0],[2,8,0],[2,9,0],[2,10,3],[2,11,2],[2,12,1],[2,13,9],[2,14,8],[2,15,10],[2,16,6],[2,17,5],[2,18,5],[2,19,5],[2,20,7],[2,21,4],[2,22,2],[2,23,4],[3,0,7],[3,1,3],[3,2,0],[3,3,0],[3,4,0],[3,5,0],[3,6,0],[3,7,0],[3,8,1],[3,9,0],[3,10,5],[3,11,4],[3,12,7],[3,13,14],[3,14,13],[3,15,12],[3,16,9],[3,17,5],[3,18,5],[3,19,10],[3,20,6],[3,21,4],[3,22,4],[3,23,1],[4,0,1],[4,1,3],[4,2,0],[4,3,0],[4,4,0],[4,5,1],[4,6,0],[4,7,0],[4,8,0],[4,9,2],[4,10,4],[4,11,4],[4,12,2],[4,13,4],[4,14,4],[4,15,14],[4,16,12],[4,17,1],[4,18,8],[4,19,5],[4,20,3],[4,21,7],[4,22,3],[4,23,0],[5,0,2],[5,1,1],[5,2,0],[5,3,3],[5,4,0],[5,5,0],[5,6,0],[5,7,0],[5,8,2],[5,9,0],[5,10,4],[5,11,1],[5,12,5],[5,13,10],[5,14,5],[5,15,7],[5,16,11],[5,17,6],[5,18,0],[5,19,5],[5,20,3],[5,21,4],[5,22,2],[5,23,0],[6,0,1],[6,1,0],[6,2,0],[6,3,0],[6,4,0],[6,5,0],[6,6,0],[6,7,0],[6,8,0],[6,9,0],[6,10,1],[6,11,0],[6,12,2],[6,13,1],[6,14,3],[6,15,4],[6,16,0],[6,17,0],[6,18,0],[6,19,0],[6,20,1],[6,21,2],[6,22,2],[6,23,6]];
-
-          data = data.map(function (item) {
-            return [item[1], item[0], item[2] || '-'];
-          });
-
-          option = {
-            tooltip: {
-              position: 'top'
-            },
-            animation: false,
-            grid: {
-              height: '50%',
-              y: '10%'
-            },
-            xAxis: {
-              type: 'category',
-              data: hours,
-              splitArea: {
-                show: true
-              }
-            },
-            yAxis: {
-              type: 'category',
-              data: days,
-              splitArea: {
-                show: true
-              }
-            },
-            visualMap: {
-              min: 0,
-              max: 10,
-              calculable: true,
-              orient: 'horizontal',
-              left: 'center',
-              bottom: '15%'
-            },
-            series: [{
-              name: 'Punch Card',
-              type: 'heatmap',
-              data: data,
-              label: {
-                normal: {
-                  show: true
-                }
-              },
-              itemStyle: {
-                emphasis: {
-                  shadowBlur: 10,
-                  shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-              }
-            }]
-          };;
-          if (option && typeof option === "object") {
-            myChart.setOption(option, true);
-          }
+  export default {
+    name: "echartDemo",
+    data() {
+      return {
+        dialogVisible: false,
+        values: {},
+        current: [],
+        option:{},
+        form: {
+          price: '',
+          upname: '',
+          downname: ''
         }
+      }
+    },
+    methods: {
+      setdata(){
+        //获取xy轴字段
+        xline = stations.obj.map((item, index) => {
+          return item.stationAlias
+        });
+        yline = [].concat(xline).reverse();
+        //坐标赋值
+        pricelist.obj.forEach((price) => {
+          let len = xline.length;
+          let y = len - price.downNo;
+          let x = price.upNo - 1;
+          values.push([x, y, price.price])
+
+        })
+      },
+      confirm() {
+        this.dialogVisible = false;
+        this.option.series[0].data=[[0,0,'8.00']];
+        this.values.price=this.form.price;
+        myChart.clear();
+        this.drawPic()
+      },
+      drawPic(){
+        let  that = this;
+         setTimeout(()=>{
+           myChart.setOption(that.option, true);
+         },100)
+
+      }
+    },
+    mounted() {
+      dom = document.getElementById("main");
+
+      myChart = echarts.init(dom);
+      let app = {};
+      let option = null;
+      app.title = '上下行车票价统计';
+      this.setdata();
+
+      option={
+        tooltip: {
+          position: 'top',
+            formatter: function (a) {
+            let index = xline.indexOf(a.name);
+            let yname = yline[index];
+            return a.seriesName + '<br/>' + '上车：' + a.name + '<br/>' + '下车：' + yname + '<br>票价：' + Number(a.value[2]).toFixed(2) + '（元）'
+          }
+        },
+        animation: false,
+          grid: {
+          height: '50%',
+            y: '10%'
+        },
+        xAxis: {
+          nameLocation: 'center',
+            nameGap: 50,
+            name: '上行站',
+            type: 'category',
+            data: xline,
+            splitArea: {
+            show: true
+          },
+          triggerEvent: true
+        },
+        yAxis: {
+          nameLocation: 'center',
+            nameGap: 70,
+            name: '下行站',
+            type: 'category',
+            data: yline,
+            splitArea: {
+            show: true
+          }
+        },
+        visualMap: {
+          min: 0,
+            max: 5,
+            calculable: true,
+            orient: 'horizontal',
+            left: 'center',
+            bottom: '15%'
+        },
+        series: [{
+          name: '站点信息',
+          type: 'heatmap',
+          data: values,
+          label: {
+            normal: {
+              show: true
+            }
+          },
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }]
+      };
+      this.option=option;
+      if (option && typeof option === "object") {
+        this.drawPic()
+        myChart.on('click', (params) => {//params =》[x,y,值]
+          let upname = params.name;        //上行站名
+          let currentval  =  params.value; //下行站名
+          let index = currentval[1];
+          let downname = yline[index];
+          let price = params.value[2];
+          this.form = {
+            price,
+            upname,
+            downname
+          }
+          this.option.series.data=[];
+          this.current =params.value;
+          this.dialogVisible = true;
+          let downid='';
+          let upid='';
+          stations.obj.forEach((item)=>{
+            if(item.stationAlias==downname){
+              downid= item.id;
+            }
+            if(item.stationAlias==upname){
+              upid= item.id;
+            }
+          });
+          this.values={
+            downname,
+            upname,
+            downid,
+            upid
+          }
+
+        });
+      }
+    },
+    updated(){
+
+
+
     }
+  }
 </script>
 
 <style scoped lang="less">
- #main{
-   width: 100%;
-   height: 500px;
- }
+  #main {
+    width: 100%;
+    height: 500px;
+  }
 </style>
